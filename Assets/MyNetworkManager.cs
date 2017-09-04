eb91a4ff814fb481e6bd2c00d87acf02fd161ed8
jesus lopez <jelz2yk@hotmail.com>
2017-09-04 14:03:34 +0300

    fisrt commit

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+   
+    // Use this for initialization
+    void Start () {
+   
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MyStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " staring host");
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad +  " host started");
+        Debug.Log("started");
+    }
+
+    public override void OnStartClient(NetworkClient client)
+    {
+        base.OnStartClient(client);
+
+        Debug.Log(Time.timeSinceLevelLoad + " client start requested");
+        InvokeRepeating("PrintDots", 0f, 1f);
+
+    }
+
+    
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnStartClient(client);
+
+        Debug.Log(Time.timeSinceLevelLoad + " client connecting to ip: " + conn.address);
+        CancelInvoke();
+       
+
+    }
+
+    void PrintDots()
+    {
+        Debug.Log(".");
+    }
+
+}
---
+++ Assets/PLayers/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    private Vector3 inputValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+        inputValue.y = 0f;
+        inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Debug.Log(inputValue.x +  " " + inputValue.z);
+        transform.Translate(inputValue);
+    }
+}
